9b922f9c1acf894686a9c93e434c6e3be35d266a
ilake <lake.ilakela+github@gmail.com>
2014-06-01 01:45:52 +0800

    improve to let it not only could be use in rails.

--- rails_foo/app/middleware/dummy_middleware.rb
+++ rails_foo/app/middleware/dummy_middleware.rb
@@ -1,20 +1,31 @@
 class DummyMiddleware
-  def initialize(app)
+  def initialize(app, message = "Response")
     @app = app
+    @message = message
   end
 
   def call(env)
     Rails.logger.info('Request start------')
-    status, headers, response = @app.call(env)
+    @status, @headers, @response = @app.call(env)
 
     # We could get the response body, that should be some rack wrapper it.
+    # assume it is ActionController::Response object
     # result = response.body
 
     # So when we insert the middleware in the first one ( config.middleware.insert_before 0, 'DummyMiddleware' ),
     # we could not get the response.body
-    result = response.each_with_object("") {|part, body| body << part}
+    # result = response.each_with_object("") {|part, body| body << part}
 
+    [@status, @headers, self]
 
-    [status, headers, [ "<!-- Response Time: ... -->\n", result, "\n<!-- This is content End -->" ]]
+    # [status, headers, [ "<!-- Response Time: ... -->\n", result, "\n<!-- This is content End -->" ]]
+  end
+
+  # The only method that Rack requires a response object to support is each
+  # This makes our DummyMiddleware class a type of response
+  # http://rack.rubyforge.org/doc/Rack/Response.html
+  def each(&block)
+    block.call("<!-- #{@message}: -->\n")# if @headers["Content-Type"].include? "text/html"
+    @response.each(&block)
   end
 end
