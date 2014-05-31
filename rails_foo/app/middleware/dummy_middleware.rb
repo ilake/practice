afef2d532546a6d22a0a0aaa2e670d4e91add91c
ilake <lake.ilakela+github@gmail.com>
2014-05-31 15:15:52 +0800

    play middleware.

--- rails_foo/app/middleware/dummy_middleware.rb
+++ rails_foo/app/middleware/dummy_middleware.rb
@@ -5,7 +5,16 @@ class DummyMiddleware
 
   def call(env)
     Rails.logger.info('Request start------')
-    @app.call(env)
-    Rails.logger.info('Request End------')
+    status, headers, response = @app.call(env)
+
+    # We could get the response body, that should be some rack wrapper it.
+    # result = response.body
+
+    # So when we insert the middleware in the first one ( config.middleware.insert_before 0, 'DummyMiddleware' ),
+    # we could not get the response.body
+    result = response.each_with_object("") {|part, body| body << part}
+
+
+    [status, headers, [ "<!-- Response Time: ... -->\n", result, "\n<!-- This is content End -->" ]]
   end
 end
