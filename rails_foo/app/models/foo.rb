23b53f6349a4509ca4b9bcec52ea57eec086555d
ilake <lake.ilakela+github@gmail.com>
2014-05-31 23:56:35 +0800

    more callback.

--- rails_foo/app/models/foo.rb
+++ rails_foo/app/models/foo.rb
@@ -1,11 +1,34 @@
 # http://api.rubyonrails.org/classes/ActiveSupport/Callbacks.html
+# http://api.rubyonrails.org/classes/ActiveSupport/Callbacks/ClassMethods.html
+
+class Bar
+  def before(caller)
+    puts "Bar before called"
+  end
+
+  def before_foo(caller)
+    puts "Bar before_foo called"
+  end
+
+  def foo(caller)
+    puts "Bar foo called"
+  end
+end
+
 class Foo
   include ActiveSupport::Callbacks
 
   CALLBACK_NAME = :foo
 
-  define_callbacks CALLBACK_NAME
-  set_callback CALLBACK_NAME, :before, :before_callback
+  define_callbacks CALLBACK_NAME, terminator: ->(target, result) { target.skip_callback },
+                                  skip_after_callbacks_if_terminated: true, scope: [:name]
+  # only one will be trigger
+  # set_callback CALLBACK_NAME, :before, :before_callback
+  set_callback CALLBACK_NAME, :before, ::Bar.new
+
+  def skip_callback
+    true
+  end
 
   def before_callback
     puts "before trigger foo"
