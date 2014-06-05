096dc89f6797852db6fa544e093445314e56504a
ilake <lake.ilakela+github@gmail.com>
2014-06-05 18:33:48 +0800

    add search file feature and find the real latest mod.

--- civ5_subscribe_file.rb
+++ civ5_subscribe_file.rb
@@ -1,17 +1,30 @@
 require 'fileutils'
+require 'ruby-debug'
 
-plugins_folder = "#{ENV['HOME']}/Library/Application Support/Steam/userdata"
-plugins = "#{plugins_folder}*"
+begin
+  plugins_folder = "#{ENV['HOME']}/Library/Application Support/Steam/userdata"
+  plugins = "#{plugins_folder}/*"
 
-latest_download_plugin_folder_path = Dir[plugins_folder].sort_by{ |f| File.ctime(f) }.first
+  latest_download_plugin_folder_path = Dir[plugins].sort_by{ |f| File.ctime(f) }.first
+  last_download_plugin_folder_path = Dir["#{ latest_download_plugin_folder_path }/ugc/referenced/*"].sort_by{ |f| File.ctime(f) }.last
 
-match_mod_file = Dir.glob("#{latest_download_plugin_folder_path}/**/*.civ5mod").first
-if match_mod_file
-  FileUtils.cp(match_mod_file, "#{ENV['HOME']}/Desktop")
-  `open '/Applications/StuffIt Expander.app'`
-  puts "Pull the mod file in desktop to StuffIt Expander"
-else
-  puts "Could not find the latest mod file"
-  `open #{plugins_folder}`
+  match_mod_file = if $*.any?{|input| input =~ /file/}
+    query_file = $*.any?{|input| input =~ /file/} ? $*.find{|input| input =~ /file=(.*)/}.gsub('file=', '') : ''
+    Dir.glob("#{latest_download_plugin_folder_path}/**/*#{query_file}*.civ5mod").first
+  else
+    Dir.glob("#{last_download_plugin_folder_path}/**/*#{query_file}*.civ5mod").first
+  end
+
+  if match_mod_file
+    FileUtils.cp(match_mod_file, "#{ENV['HOME']}/Desktop")
+    `open '/Applications/StuffIt Expander.app'`
+    puts "Pull the mod file in desktop to StuffIt Expander"
+    `open '#{plugins_folder}'` if $*.include?('open')
+  else
+    puts "Could not find the latest mod file"
+    `open '#{plugins_folder}'`
+  end
+
+rescue
+  exit
 end
-
