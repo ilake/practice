01316e8dc90621c8c4844cd106b7300887a1ce6d
ilake <lake.ilakela+github@gmail.com>
2014-09-08 01:06:15 +0800

    use unarchiver CLI to unarchive mod5

--- civ5_subscribe_file.rb
+++ civ5_subscribe_file.rb
@@ -1,7 +1,10 @@
 require 'fileutils'
 require 'ruby-debug'
+require 'pathname'
+require 'shellwords'
 
 begin
+  default_mod_path = ENV['MOD_PATH'] || "#{ENV['HOME']}/Documents/Aspyr/Sid Meier's Civilization 5/MODS/"
   plugins_folder = "#{ENV['HOME']}/Library/Application Support/Steam/userdata"
   plugins = "#{plugins_folder}/*"
 
@@ -17,9 +20,12 @@ begin
 
   if match_mod_file
     FileUtils.cp(match_mod_file, "#{ENV['HOME']}/Desktop")
-    `open '/Applications/StuffIt Expander.app'`
-    puts "Pull the mod file in desktop to StuffIt Expander"
-    `open '#{plugins_folder}'` if $*.include?('open')
+    # http://unarchiver.c3.cx/commandline
+    unarchive_command = `which unar`
+
+    puts "extract mod #{Pathname.new(match_mod_file).basename.to_s} to #{default_mod_path}"
+    `#{unarchive_command.strip} -f #{Shellwords.escape(match_mod_file.strip)} -o #{Shellwords.escape(default_mod_path.strip)}`
+    `open #{Shellwords.escape(default_mod_path)}`
   else
     puts "Could not find the latest mod file"
     `open '#{plugins_folder}'`
