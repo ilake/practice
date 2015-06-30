337eedafd2fbf62d699f6d2fb46b543259c3c13c
ilake <lake.ilakela+github@gmail.com>
2015-07-01 06:14:45 +0800

    Init and add Tab

---
+++ BookSearch/Featured.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+    StyleSheet,
+    View,
+    Text,
+    Component
+   } = React;
+
+var styles = StyleSheet.create({
+    description: {
+        fontSize: 20,
+        backgroundColor: 'white'
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+class Featured extends Component {
+    render() {
+        return (
+  	    <View style={styles.container}>
+	        <Text style={styles.description}>
+        	  Featured Tab
+	        </Text>
+	    </View>
+        );
+    }
+}
+
+// exports the Featured class thus making it available for use by other files
+module.exports = Featured;
