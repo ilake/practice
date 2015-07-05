41333981322919130ab04420404b2c046b222bf2
ilake <lake.ilakela+github@gmail.com>
2015-07-05 11:10:44 +0800

    Use FBLogin Component

--- FBLoginComponent/index.ios.js
+++ FBLoginComponent/index.ios.js
@@ -5,6 +5,9 @@
 'use strict';
 
 var React = require('react-native');
+var FBLogin = require('react-native-facebook-login');
+var FBLoginManager = require('NativeModules').FBLoginManager;
+
 var {
   AppRegistry,
   StyleSheet,
@@ -15,18 +18,38 @@ var {
 var FBLoginComponent = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <FBLogin style={{ marginBottom: 10, }}
+        permissions={["email","user_friends"]}
+        onLogin={function(data){
+          console.log("Logged in!");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLogout={function(){
+          console.log("Logged out.");
+          _this.setState({ user : null });
+        }}
+        onLoginFound={function(data){
+          console.log("Existing login found.");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLoginNotFound={function(){
+          console.log("No user logged in.");
+          _this.setState({ user : null });
+        }}
+        onError={function(data){
+          console.log("ERROR");
+          console.log(data);
+        }}
+        onCancel={function(){
+          console.log("User cancelled.");
+        }}
+        onPermissionsMissing={function(data){
+          console.log("Check permissions!");
+          console.log(data);
+        }}
+      />
     );
   }
 });
